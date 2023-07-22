8363af6eebac5731f6afcdbae1c5db0c5a5a17fe
SyedYakhub <syedyakhub9966@gmail.com>
2023-07-22 17:26:26 +0530

    added command to view grafana admin user password

--- prometheus+grafana/prometheus+grafana_helm_launch.sh
+++ prometheus+grafana/prometheus+grafana_helm_launch.sh
@@ -18,3 +18,10 @@ kubectl patch service prometheus-grafana -p '{"spec": {"type": "NodePort"}}'
 
 #Else you can expose extra svc with the below details
 #kubectl expose service prometheus-server --type=NodePort --target-port=9090 --name=prometheus-server-ext
+
+# The command will output the base64 encoded password. Use base64 --decode to decode it and reveal the admin password.
+# Once you have the admin password, you can access the Grafana web interface using the NodePort or LoadBalancer IP exposed by the Grafana service and log in with the "admin" username and the password obtained from the previous step.
+
+# Here's an example of how the command would look like:
+
+# kubectl get secret --namespace default prometheus-grafana -o jsonpath="{.data.admin-password}" | base64 --decode
