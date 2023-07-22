a9ec9fa71f75cc6d1eb44f7a649c61d3a5820624
SyedYakhub <syedyakhub9966@gmail.com>
2023-07-22 16:52:49 +0530

    modifies files

--- prometheus+grafana/prometheus+grafana_helm_launch.sh
+++ prometheus+grafana/prometheus+grafana_helm_launch.sh
@@ -11,6 +11,7 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update
 helm install prometheus prometheus-community/kube-prometheus-stack
 kubectl patch service prometheus-kube-prometheus-prometheus -p '{"spec": {"type": "NodePort"}}'
+kubectl patch service prometheus-grafana -p '{"spec": {"type": "NodePort"}}'
 
 #Alternatively you can also edit service and change type=NodePort using below command
 #kubectl edit svc prometheus-server
